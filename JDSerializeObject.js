02c7f48c5ebc9c3421eb916472d15e521257a147
田继东 <tianjidong@jd.com>
2018-05-09 15:14:57 +0800

    新增对数组的支持

--- JDSerializeObject.js
+++ JDSerializeObject.js
@@ -11,25 +11,69 @@
          */
         var extend = function (parent, curElement) {
 
-            //如果只是普通节点，没有父子节点的话，直接就绑定对应的值，不做其他处理
-            if (curElement.name.indexOf(".") === -1) {
+            //用来判断是否是 student[0] 这种
+            var arrayNameRegex = /^[a-zA-Z]+[0-9]*\[[0-9]+]$/;
+            var hasSon = curElement.name.indexOf(".") !== -1;
+            var isArray = hasSon ? arrayNameRegex.test(curElement.name.substring(0,curElement.name.indexOf("."))) : arrayNameRegex.test(curElement.name);
+            //一，如果只是普通节点，没有父子节点并且不是xxx[0] 这种，直接就绑定对应的值，不做其他处理
+            //例如：name=John
+            if (!hasSon && !isArray) {
                 bind(parent, curElement);
                 return;
             }
 
-            var firstLevelElementName = curElement.name.substring(0,curElement.name.indexOf("."));
-            var firstLevelNode = parent[firstLevelElementName];
-            if('undefined' === typeof firstLevelNode || firstLevelNode === null){
-                firstLevelNode = {};
+            if(isArray){
+                var arrayName = curElement.name.substring(0,curElement.name.indexOf("["));
+                var curValueArrayIndex = parseInt(curElement.name.substring(curElement.name.indexOf("[")+1 , curElement.name.indexOf("]")));
+                var arrayNode = parent[arrayName];
+                if(hasSon){
+                    //有子节点的
+                    //例如：order[0].price=123
+                    if('undefined' === typeof arrayNode || arrayNode === null){
+                        arrayNode = new Array(curValueArrayIndex + 1);
+                    }
+                    if(!$.isArray(arrayNode)){
+                        arrayNode = [arrayNode];
+                    }
+                    var sonElement = {
+                        name : curElement.name.substring(curElement.name.indexOf(".") + 1),
+                        value : curElement.value
+                    };
+                    //1，先递归下去，把数组中当前这个元素组装好
+                    var curArrayObj = arrayNode[curValueArrayIndex];
+                    if('undefined' === typeof curArrayObj || curArrayObj === null){
+                        curArrayObj = {}
+                    }
+                    extend(curArrayObj,sonElement);
+                    //2，把组装好的当前数组元素根据下标放到数组中
+                    arrayNode[curValueArrayIndex] = curArrayObj;
+                    //3，把当前数组放到parent中
+                    parent[arrayName] = arrayNode;
+                }else {
+                    //无子节点
+                    //例如：name[0]=John
+                    if('undefined' === typeof arrayNode || arrayNode === null){
+                        arrayNode = new Array(curValueArrayIndex + 1);
+                    }
+                    if(!$.isArray(arrayNode)){
+                        arrayNode = [arrayNode];
+                    }
+                    arrayNode[curValueArrayIndex] = curElement.value;
+                    parent[arrayName] = arrayNode;
+                }
+            }else {
+                var otherSonElement = {};
+                var firstLevelNodeName = curElement.name.substring(0,curElement.name.indexOf("."));
+                var firstLevelNode = parent[firstLevelNodeName];
+                if('undefined' === typeof firstLevelNode || firstLevelNode === null){
+                    firstLevelNode = {};
+                }
+                otherSonElement.name = curElement.name.substring(curElement.name.indexOf(".") + 1);
+                otherSonElement.value = curElement.value;
+
+                extend(firstLevelNode,otherSonElement);
+                parent[firstLevelNodeName] = firstLevelNode;
             }
-            var otherSonElement = {
-                name:curElement.name.substring(curElement.name.indexOf(".") + 1),
-                value:curElement.value
-            };
-
-            extend(firstLevelNode,otherSonElement);
-
-            parent[firstLevelElementName] = firstLevelNode;
         };
         /**
          * 绑定某个节点
